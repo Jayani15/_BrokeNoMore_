725685faa86424f1aa828395a60a7551fd3de7a2
Immidi Venkata Sai Sri Lakshmi Jayani <jayaniimmidi@gmail.com>
2025-06-12 03:05:18 +0530

    Add files via upload

--- Frontend/js/delete.js
+++ Frontend/js/delete.js
@@ -18,23 +18,3 @@ function deleteItem(id) {
     });
   }
 }
-function deleteItem(id) {
-  if (confirm("Are you sure you want to delete this item?")) {
-    fetch('delete_item.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'id=' + encodeURIComponent(id)
-    })
-    .then(response => response.text())
-    .then(data => {
-      if (data === "success") {
-        const row = document.getElementById("row-" + id);
-        if (row) row.remove(); 
-      } else {
-        alert("Failed to delete item.");
-      }
-    });
-  }
-}
